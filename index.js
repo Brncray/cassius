0ab111ce99b4deeada0ccc7292586423b634a142
Brncray <57870234+Brncray@users.noreply.github.com>
2021-04-13 12:48:57 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const brnclient = require("./structures/brnClient")
 const bl1tz = require ("./structures/bl1tz")
 const brnClient2 = require ("./structures/brnClient2")
 const himma = require ("./structures/himmaclient")
-new brnclient().start("ODIxNDU4NDg5OTY3OTAyNzIw.YFEA1Q.N5-dm6mDhk3oZE9a43gfwuZlfMU", "./brmcommands") //Brncray1 Token
-new bl1tz().start("ODE0NTg1OTUxNDgwOTcxMjY1.YDgASQ.ADNRiw7MNX4SrBJ--yJm6OJEQIQ", "./bl1tzcommands") //Brncray other bot
-new brnClient2().start("ODIyMTcxNTIxNjYwNzQ3ODQ3.YFOY5Q.yyBXDRB_TpR8cbxkMZm2QWz96Ws", "./brncommands2")// Brncray's 3rd bot.
-new himma().start("ODIyNTA0MTgzODY0NzU0MTc2.YFTOtg.O--wvL7GtNmmkHk7dHd3XWPHrPE", "./himmautils")// Brncray's 4th bot.
-new erlc().start("ODI4NDI1MTYyMDUxMTU4MDU3.YGpZDg.0aW8j2898qlYWXeL6e54Owj4zE4", "./erlc")
+new brnclient().start("", "./brmcommands") //Brncray1 Token
+new bl1tz().start("", "./bl1tzcommands") //Brncray other bot
+new brnClient2().start("", "./brncommands2")// Brncray's 3rd bot.
+new himma().start("", "./himmautils")// Brncray's 4th bot.
+new erlc().start("", "./erlc")
